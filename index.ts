87b1f4149ea00091f47e3a6ae88d97e6e9633102
matheustodao <todaomatheus@hotmail.com>
2023-01-02 01:05:57 -0300

    refactor: add static route

--- index.ts
+++ index.ts
@@ -3,18 +3,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 import { router } from './router';
+import path from 'node:path';
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
+    const PORT = 3001;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
     app.use(express.json());
 
     app.use(router);
 
-    const PORT = 3001;
     app.listen(PORT, () => console.debug(`Server Started at https://localhost:${PORT}`));
   })
   .catch((err) => console.log(`Mongo is failed: ${err}`));
