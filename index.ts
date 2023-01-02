54ce918c1907bc0901599c9612074e7cfb9ab1a8
matheustodao <todaomatheus@hotmail.com>
2023-01-01 23:05:12 -0300

    chore: use routes

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './router';
+
 mongoose.set('strictQuery', false);
 
 mongoose.connect('mongodb://localhost:27017')
-    .then(() => {
-        const app = express();
-        app.use(express.json());
+  .then(() => {
+    const app = express();
 
-        const PORT = 3001;
-        app.listen(PORT, () => console.debug(`Server Started at https://localhost:${PORT}`));
-    })
-    .catch((err) => console.log(`Mongo is failed: ${err}`));
+    app.use(express.json());
+
+    app.use(router);
+
+    const PORT = 3001;
+    app.listen(PORT, () => console.debug(`Server Started at https://localhost:${PORT}`));
+  })
+  .catch((err) => console.log(`Mongo is failed: ${err}`));
